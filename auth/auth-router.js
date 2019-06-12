85f20905963a8afbd8d23e54331a0daa6b89b454
harjothkhara <harjoth.khara@gmail.com>
2019-06-12 13:09:07 -0700

    notes

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken'); // <<<<<<<<<<<<<<<<<<<<<<<<<
 
 const Users = require('../users/users-model.js');
+const secrets = require('../config/secrets.js');
 
 // for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
@@ -25,8 +27,10 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user); // <<<<<<<<<<<<<<<<<<<<<<<<
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token, //<<<<<<<<<<<<<<<<<<<<<<<<
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +40,20 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) { // <<<<<<<<<<<<<<<<<<<<<<<<<<<<
+const payload = { //data you want to store in the token
+  subject: user.id, //what the token is describing
+  username: user.username,
+  roles: [ 'student' ], //if we had a generated table it would be user.roles
+};
+
+
+const options = {
+  expiresIn: '1h',
+};
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
 
 module.exports = router;
--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,23 @@
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const Users = require('../users/users-model.js');
+const secrets = require('../config/secrets.js');
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
+  const token = req.headers.authorization; //read the token from the authorization header
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
-  } else {
-    res.status(400).json({ message: 'No credentials provided' });
-  }
+  jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => { //called the verify method passing token and secrets,  
+    if(err) {
+      // token is not valid or expired
+      res.status(401).json({ you: 'shall not pass!!!' })
+    } else {
+      // the token is valid and we can read the decodedToken
+      req.decodedToken = decodedToken; //grabbed the decoded token on success and if  there was an error "you shall not pass"... error could be changing the token
+
+      next();
+    }
+  });
 };
+
+// api/users -> grabbed the token from login and added it to the headers with the authorization as the key.
+
+// users---> roles <-----> permissions
